558018a7dec14229b396690c3326126f4b91ea60
LiyangSong <song.liy@northeastern.edu>
2023-05-01 05:22:55 -0400

    initialize templates for algorithms

---
+++ src/algorithms/InsertionSort.js
@@ -0,0 +1,7 @@
+export default function InsertionSort() {
+    return(
+        <p>
+            Insertion sort is a sorting algorithm that places an unsorted element at its suitable place in each iteration.
+        </p>
+    )
+}
---
+++ src/algorithms/SelectionSort.js
@@ -0,0 +1,7 @@
+export default function SelectionSort() {
+    return(
+        <p>
+            Selection sort is a sorting algorithm that selects the smallest element from an unsorted list in each iteration and places that element at the beginning of the unsorted list.
+        </p>
+    )
+}
