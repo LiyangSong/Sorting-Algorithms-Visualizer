57fc28f7bf072b7973be4aeb92ec771e1f38cf44
LiyangSong <song.liy@northeastern.edu>
2023-05-08 19:49:57 -0400

    add main page template

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom';
+import {BrowserRouter, Route, Routes, Outlet, Navigate} from 'react-router-dom';
 import './App.css'
 import Header from './Header.js'
 import ContentProvider from "./ContentProvider.js";
+import Home from './Home.js'
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Layout />}>
+                    <Route path="home" element={<Home />} />
+                    <Route path="/" element={<Navigate to="/home" />} />
                     <Route path="bubble-sort" element={<ContentProvider sortType="bubbleSort" />} />
                     <Route path="insertion-sort" element={<ContentProvider sortType="insertionSort" />} />
                     <Route path="selection-sort" element={<ContentProvider sortType="selectionSort" />} />
