74008f253137de3ad40236f21d1fd251745b8a79
LiyangSong <song.liy@northeastern.edu>
2023-05-10 03:30:25 -0400

    add length button icons

--- src/InputArea.js
+++ src/InputArea.js
@@ -1,3 +1,5 @@
+import reduceIcon from './assets/reduce.png';
+import addIcon from './assets/add.png';
 export default function InputArea({
     currentNumbers,
     onStartSorting,
@@ -7,17 +9,25 @@ export default function InputArea({
 }) {
     return(
         <div className="inputArea">
-            <div>
-                <button onClick={onReduceLength}>－</button>
-                {currentNumbers.map(number => (
-                    <input
-                        key={number.id}
-                        onChange={(e) => {
-                            onInputNumber(number.id, e.target.value)
-                        }}
-                    />
-                ))}
-                <button onClick={onAddLength}>＋</button>
+            <div className="inputButtons">
+                <button onClick={onReduceLength} className="reduceButton">
+                    <img src={reduceIcon} alt="Reduce Length Icon" height="30px" />
+                </button>
+                <div className="spaceHolder" />
+                <div className="inputs">
+                    {currentNumbers.map(number => (
+                        <input
+                            key={number.id}
+                            onChange={(e) => {
+                                onInputNumber(number.id, e.target.value)
+                            }}
+                        />
+                    ))}
+                </div>
+                <div className="spaceHolder" />
+                <button onClick={onAddLength} className="addButton">
+                    <img src={addIcon} alt="Add Length Icon" height="30px" />
+                </button>
             </div>
             <button onClick={onStartSorting} className="startSortingButton">Start Sorting</button>
         </div>
