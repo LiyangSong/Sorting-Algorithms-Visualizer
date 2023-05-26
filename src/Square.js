faa3a620322637580ace048aa2571e6eb8d79f13
LiyangSong <song.liy@northeastern.edu>
2023-05-26 04:59:40 -0400

    add pointer to square

--- src/Square.js
+++ src/Square.js
@@ -1,13 +1,19 @@
 import { Flipped } from "react-flip-toolkit";
+import pointerIcon from "./assets/pointer.png";
 
 export default function Square({ number, flipId }) {
     return(
         <Flipped
             flipId={flipId}
-            key={flipId}
         >
-            <div className={number.isActive ? "activeSquare" : number.isSorted ? "sortedSquare" : "square"}>
-                {number.number}
+            <div className="squareArea">
+                <div className={number.isActive ? "activeSquare" : number.isSorted ? "sortedSquare" : "square"}>
+                    {number.number}
+                </div>
+                <div className="pointer">
+                    {number.isPointed &&
+                        <img src={pointerIcon} alt="Pointer Icon" height="28px" />}
+                </div>
             </div>
         </Flipped>
     )
