f18799347b7211ff095b6e47d1267e8d6d9125cf
LiyangSong <song.liy@northeastern.edu>
2023-06-13 08:13:40 -0400

    heap sort template

--- src/algorithms/heapSort.js
+++ src/algorithms/heapSort.js
@@ -1,5 +1,37 @@
-export default function heapSort(numbers, step) {
-    let nextNumbers = numbers.slice();
+import { deepCopy, swap } from "../utils";
 
-    return nextNumbers;
+export default function heapSortResult(startNumbers) {
+
+    let result = [{
+        step: 0,
+        numbers: startNumbers,
+        log: "Start Numbers."
+    }];
+    let currentStep = 0;
+    let currentNumbers = deepCopy(startNumbers);
+
+    for (let n = 0; n < currentNumbers.length; n++) {
+        currentNumbers[n].isHeaped = true;
+    }
+
+    console.log(currentNumbers);
+    result.push({
+        step: currentStep + 1,
+        numbers: currentNumbers,
+        log: "Build the heap tree."
+    })
+
+    // heapSort(currentNumbers);
+
+    // for (let n = 0; n < currentNumbers.length; n++) {
+    //     currentNumbers[n].isSorted = true;
+    // }
+    //
+    // result.push({
+    //     step: currentStep + 1,
+    //     numbers: deepCopy(currentNumbers),
+    //     log: "All set.\nSorting completed.",
+    // });
+
+    return result;
 }
