48ebc5e9d1ddeedcfc8aa1891aca441de8cd1d34
LiyangSong <song.liy@northeastern.edu>
2023-06-13 08:20:40 -0400

    code format change

--- src/AnimationArea.js
+++ src/AnimationArea.js
@@ -45,28 +45,28 @@ export default function AnimationArea({ currentNumbers, isHeaped }) {
                     <svg>
                         <line x1="212" y1="18" x2="120" y2="68" />
                         {currentNumbers.length > 2 &&
-                        <line x1="212" y1="18" x2="304" y2="68" />
+                            <line x1="212" y1="18" x2="304" y2="68" />
                         }
                         {currentNumbers.length > 3 &&
                             <line x1="120" y1="68" x2="74" y2="118" />
                         }
                         {currentNumbers.length > 4 &&
-                        <line x1="120" y1="68" x2="166" y2="118" />
+                            <line x1="120" y1="68" x2="166" y2="118" />
                         }
                         {currentNumbers.length > 5 &&
-                        <line x1="304" y1="68" x2="258" y2="118" />
+                            <line x1="304" y1="68" x2="258" y2="118" />
                         }
                         {currentNumbers.length > 6 &&
-                        <line x1="304" y1="68" x2="350" y2="118" />
+                            <line x1="304" y1="68" x2="350" y2="118" />
                         }
                         {currentNumbers.length > 7 &&
-                        <line x1="74" y1="118" x2="48" y2="168" />
+                            <line x1="74" y1="118" x2="48" y2="168" />
                         }
                         {currentNumbers.length > 8 &&
-                        <line x1="74" y1="118" x2="94" y2="168" />
+                            <line x1="74" y1="118" x2="94" y2="168" />
                         }
                         {currentNumbers.length > 9 &&
-                        <line x1="166" y1="118" x2="140" y2="168" />
+                            <line x1="166" y1="118" x2="140" y2="168" />
                         }
                     </svg>
                 </div>
