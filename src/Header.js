2eca8a75b952a6ac7c1b70796600b3ac03b9cc7c
LiyangSong <song.liy@northeastern.edu>
2023-08-15 03:41:21 -0400

    add comments for header and remove unnecessary subcomponents

--- src/Header.js
+++ src/Header.js
@@ -10,6 +10,7 @@ import quickSortIcon from './assets/quick.png';
 import mergeSortIcon from './assets/merge.png';
 import heapSortIcon from './assets/heap.png';
 
+// Route path, name, and icon for each sorting algorithm.
 export const tabs = [
     { path: "/bubble-sort", label: "Bubble Sort", icon: bubbleSortIcon },
     { path: "/insertion-sort", label: "Insertion Sort", icon: insertionSortIcon },
@@ -19,10 +20,18 @@ export const tabs = [
     { path: "/heap-sort", label: "Heap Sort", icon: heapSortIcon },
 ];
 
+/**
+ * A header component includes logo area, tab menu, and a link to GitHub repo.
+ * @author - Liyang
+ * @returns {JSX.Element} - The rendered JSX Component.
+ */
 export default function Header() {
+    // 'dropDown' is used as a boolean state to determine whether tab menu is expanded.
     const [dropDown, setDropDown] = useState(false);
     let tabsRef = useRef();
 
+    // When 'dropDown' changed, add an event listener to respond to mouse click or touch behavior.
+    // When click or touch areas outside tabs, set 'dropDown' as false and close expanded tabs.
     useEffect(() => {
         const handler = (e) => {
             if (dropDown && tabsRef.current && !tabsRef.current.contains(e.target)) {
@@ -37,50 +46,59 @@ export default function Header() {
         };
     }, [dropDown])
 
-    function handleDropDown() {
-        setDropDown(prev => !prev);
-    }
-
-    function closeDropDown() {
-        setDropDown(false);
-    }
-
     return(
         <nav className="header">
-            <LogoArea />
+            <a href="/home" className="header-logoArea">
+                <img
+                    src={logo}
+                    alt="Web Logo"
+                    height="50px"
+                    width="50px"
+                />
+                <div>Sorting Algorithms Visualizer</div>
+            </a>
+
             <div className="spaceHolder" />
+
             <div className="header-tabsArea" ref={tabsRef}>
-                <TabMenu onDropDown={handleDropDown}/>
-                {dropDown && <Tabs onOpenTab={closeDropDown}/>}
+                <button
+                    className="menu"
+                    onClick = {() => setDropDown(prev => !prev)}
+                >
+                    <img
+                        src={menuIcon}
+                        alt="Tab Menu"
+                        height="25px"
+                    />
+                </button>
+
+                {dropDown &&
+                    <Tabs onOpenTab = {() => setDropDown(false)}/>
+                }
             </div>
 
-            <GitLink />
-
+            <a
+                className="header-gitlink"
+                href="https://github.com/LiyangSong/Sorting-Algorithms-Visualizer"
+                target="_blank"
+                title="Visit Our GitHub Page"
+            >
+                <img
+                    src={githubLogo}
+                    alt="Github Logo"
+                    height="30px"
+                />
+            </a>
         </nav>
     )
 }
 
-function LogoArea() {
-    return(
-        <a href="/home" className="header-logoArea">
-            <img src={logo} alt="Web Logo" height="50px" width="50px"/>
-            <div>Sorting Algorithms Visualizer</div>
-        </a>
-
-    )
-}
-
-function TabMenu({ onDropDown }) {
-    return(
-        <button
-            className="menu"
-            onClick={onDropDown}
-        >
-            <img src={menuIcon} alt="Tab Menu" height="25px"/>
-        </button>
-    )
-}
-
+/**
+ * Expanded tabs to provide navigation links to different algorithm routes.
+ * @author - Liyang
+ * @param {function} onOpenTab - The function triggered by clicking one expanded tab.
+ * @returns {JSX.Element} - The rendered JSX Component.
+ */
 function Tabs({ onOpenTab }) {
     return (
         <div className="tabs">
@@ -97,19 +115,6 @@ function Tabs({ onOpenTab }) {
                 </NavLink>
             ))}
         </div>
-    )
-}
-
-function GitLink() {
-    return(
-        <a
-            className="header-gitlink"
-            href="https://github.com/LiyangSong/Sorting-Algorithms-Visualizer"
-            target="_blank"
-            title="Visit Our GitHub Page"
-        >
-            <img src={githubLogo} alt="Github Logo" height="30px"/>
-        </a>
     )
 }
 
