8c65bab0481a2da98ad1f0a68e1534f7426cfc5f
LiyangSong <song.liy@northeastern.edu>
2023-08-15 03:20:29 -0400

    remove redundant component

--- src/Footer.js
+++ src/Footer.js
@@ -7,19 +7,12 @@ export default function Footer() {
     return(
         <div className="footer">
             <p>A Project Developed by Keke, Liyang, and Yue in Summer 2023.</p>
-            <Contact />
+            <a
+                href="mailto:song.liy@northeastern.edu"
+                className="footer_contact"
+            >
+                Contact Us
+            </a>
         </div>
   );
 }
-
-/**
- * A mail link as contact information.
- * @returns {JSX.Element} - The rendered JSX Component.
- */
-function Contact() {
-    return (
-        <a href="mailto:song.liy@northeastern.edu" className="footer_contact">
-          Contact Us
-        </a>
-    );
-  }
