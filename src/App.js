64531899f7606c8ff8f3ff275ca26a53ca188e5d
LiyangSong <song.liy@northeastern.edu>
2023-08-15 03:47:23 -0400

    add comments and optimize routers config

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
 import React from 'react';
-import {BrowserRouter, Route, Routes, Outlet, Navigate} from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 import Header from './Header.js';
 import ContentProvider from "./ContentProvider.js";
 import Home from './Home.js';
 
+
+/**
+ * The root component of the web application, also defining Routes of the application.
+ * @author - Liyang
+ * @returns {JSX.Element} - The rendered JSX Component.
+ */
 export default function App() {
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route path="home" element={<Home />} />
-                    <Route path="/" element={<Navigate to="/home" />} />
-                    <Route path="bubble-sort" element={<ContentProvider sortType="bubbleSort" />} />
-                    <Route path="insertion-sort" element={<ContentProvider sortType="insertionSort" />} />
-                    <Route path="selection-sort" element={<ContentProvider sortType="selectionSort" />} />
-                    <Route path="quick-sort" element={<ContentProvider sortType="quickSort" />} />
-                    <Route path="merge-sort" element={<ContentProvider sortType="mergeSort" />} />
-                    <Route path="heap-sort" element={<ContentProvider sortType="heapSort" />} />
-                </Route>
+                <Route path="home" element={<Home />} />
+                <Route path="/" element={<Navigate to="/home" />} />
+                <Route path="bubble-sort" element={<ContentProvider sortType="bubbleSort" />} />
+                <Route path="insertion-sort" element={<ContentProvider sortType="insertionSort" />} />
+                <Route path="selection-sort" element={<ContentProvider sortType="selectionSort" />} />
+                <Route path="quick-sort" element={<ContentProvider sortType="quickSort" />} />
+                <Route path="merge-sort" element={<ContentProvider sortType="mergeSort" />} />
+                <Route path="heap-sort" element={<ContentProvider sortType="heapSort" />} />
             </Routes>
         </BrowserRouter>
-    )
-}
-
-function Layout() {
-    return(
-        <div className="layout">
-            <Header />
-            <Outlet />
-        </div>
     )
 }
 
