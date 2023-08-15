a8b721704fe54007a86f585afa8849fc1baa2aeb
LiyangSong <song.liy@northeastern.edu>
2023-08-15 03:48:44 -0400

    add comments

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 
+// Main entry of application.
+// Render component 'App' within div 'root'.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
