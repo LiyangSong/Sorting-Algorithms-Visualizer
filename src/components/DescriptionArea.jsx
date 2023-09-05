1ebd31947f7bdfe694db20dd90b4b843a00ed656
LiyangSong <song.liy@northeastern.edu>
2023-09-05 03:23:42 -0400

    replace png icons with svg format

--- src/components/DescriptionArea.jsx
+++ src/components/DescriptionArea.jsx
@@ -1,5 +1,5 @@
-import time from '../assets/time.png'
-import drive from '../assets/drive.png'
+import time from '../assets/time.svg'
+import drive from '../assets/drive.svg'
 
 /**
  * Brief description as well as time and space complexities regarding each sorting algorithm.
